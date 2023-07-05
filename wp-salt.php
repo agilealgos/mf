9abbd4831a398789fc010ad5f98da8a943802186
Harish PS <harish@psharish.com>
2023-07-05 12:22:13 +0530

    Remaining Files

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'wnmJ9gaNubYUNRMDD5vHVBNEW0cWMpeY4yXEsMCIGw94I98nGoubUDtmgUJxNQRb');
+define('SECURE_AUTH_KEY',  'TtTSb3mVnza03dn7Hmr5Pu6aRd2daftsVX6J7bgGQ5EnIrL6zSzuIv5P0nC5iEa6');
+define('LOGGED_IN_KEY',    'Bu8yUmNw46tga5SGiMYXeors7SnLNC6fPREgE0H7BI7ss5ImVyWU0P499yTWUUxJ');
+define('NONCE_KEY',        'e4Ciqdv6LW8zosQIx7YWD8WLrsyp7R88hcIP3xpJEIFSda6bQPb43dAN7YrqqWF3');
+define('AUTH_SALT',        'PsaNnK6XhFfGCRoBBqNTp26gn6eSyTTUPMczGNsDSoNFy4EDPc4xE6MgcIpfjXSg');
+define('SECURE_AUTH_SALT', '2fcpHhyavAwzmnSn6JpgvUvDfuT4X6a3N3DpmT7E1xqbpiEmdTBsjeaHbu2AxMLg');
+define('LOGGED_IN_SALT',   'CanzLdrQTjXYo3zG4y1tfjUHB3RYEvVFrVYUdEduyHJY0KFuu6bEJANR8Xdg7aMw');
+define('NONCE_SALT',       'qusXeRGHR1g4emIRMXcEHKBf9Jdab59pRePzqwTcFKhvpA0naMd7vYPW2WbA1NKW');
